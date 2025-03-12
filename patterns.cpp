f7ba2cc9ea1d4cbcb5aa342bcd8cd4a1caa7e4f3
Siddu Deshetti <143020292+Siddhu767@users.noreply.github.com>
2025-03-12 20:09:54 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -88,8 +88,49 @@ void pattern7(){
     }
 }
 
+void pattern8(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        //space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"$";
+        }
+        //space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+
+
+        cout<<endl;
+    }
+}
+
+void pattern9(){
+    pattern7();
+    pattern8();
+}
+
+void pattern10(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>=n) stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
 
 int main() {
-    pattern7();
+    pattern10();
     return 0;
 }
