1cd50749daad2381cd3eca3f4a37aaa270910acd
Siddu Deshetti <143020292+siddudeshetti@users.noreply.github.com>
2025-08-06 01:11:20 +0530

    Update bubblesort.cpp

--- Algorithms/Sorting/bubblesort.cpp
+++ Algorithms/Sorting/bubblesort.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubblesort(int n,int arr[]){
-    //bubblesort algorithm
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n-i-1;j++){
-            if (arr[j]>arr[j+1]){
-                swap(arr[j], arr[j+1]);
-            }
+void bubble_sort(vector<int> &v){
+    for(int i=0;i<v.size()-1;i++){
+        for(int j=0;j<v.size()-i-1;j++){ //optimized with not iterating end of sorted array
+            if(v[j]>v[j+1]) swap(v[j],v[j+1]);
         }
     }
-    
 }
 
-int main() {
-    int n;
-    cin>>n;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int i;
+        vector<int> v;
+        while(cin>>i){
+            v.push_back(i);
+            if(cin.peek()=='\n') break;
+        }
 
-    int arr[n];
-    for (int i=0;i<n;i++) {
-        cin>>arr[i];
-    }
+        bubble_sort(v);
 
-    bubblesort(n,arr);
+        for(auto x:v){
+            cout<<x<<" ";
+        }
 
-    for (int i=0;i<n;i++) {
-        cout<<arr[i]<<" ";
+        cout<<'\n';
     }
 }
