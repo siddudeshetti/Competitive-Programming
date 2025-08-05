9cdd147fd828c8accbec20730536f778b064b296
Siddu Deshetti <143020292+siddudeshetti@users.noreply.github.com>
2025-08-06 01:12:24 +0530

    Update selectionsort.cpp

--- Algorithms/Sorting/selectionsort.cpp
+++ Algorithms/Sorting/selectionsort.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void selectionsort(int n,int arr[]){
-    //Selectionsort algorithm
-    for (int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[i]){
-                swap(arr[i],arr[j]);
-            }
+void selection_sort(vector<int> &v){
+    for(int i=0;i<v.size()-1;i++){
+        int x=i; //pointer 1
+        for(int j=i;j<v.size()-1;j++){
+            if(v[j]<v[x]) x=j; //selecting minimum
         }
+        swap(v[x],v[i]); //swap minimum with i
     }
 }
 
-int main() {
-    int n;
-    cin>>n;
-    int arr[n];
-    for (int i=0;i<n;i++) {
-        cin>>arr[i];
-    }
-    selectionsort(n,arr);
-    for (int i=0;i<n;i++) {
-        cout<<arr[i]<<" ";
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int i;
+        vector<int> v;
+        while(cin>>i){
+            v.push_back(i);
+            if(cin.peek()=='\n') break;
+        }
+
+        selection_sort(v);
+
+        for(auto x:v){
+            cout<<x<<" ";
+        }
+
+        cout<<'\n';
     }
 }
