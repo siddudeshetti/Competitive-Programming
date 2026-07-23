cfa8e4f144ba1d139983a5e311ec03840b734601
Siddu Deshetti <143020292+siddudeshetti@users.noreply.github.com>
2026-07-23 15:40:56 +0530

    Implement Trie data structure with insert and search

---
+++ trie.py
@@ -0,0 +1,46 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    # Insert a word
+    def insert(self, word):
+        node = self.root
+
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+
+            node = node.children[ch]
+
+        node.end = True
+
+    # Search complete word
+    def search(self, word):
+        node = self.root
+
+        for ch in word:
+            if ch not in node.children:
+                return False
+
+            node = node.children[ch]
+
+        return node.end
+
+    # Check prefix
+    def startsWith(self, prefix):
+        node = self.root
+
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+
+            node = node.children[ch]
+
+        return True
