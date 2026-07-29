6a139444db4ca7a67f0271e1f74ada4b01fa9c86
Siddu Deshetti <143020292+siddudeshetti@users.noreply.github.com>
2026-07-30 03:19:37 +0530

    Comment out unused graph functions and logic
    Commented out sections of code related to graph insertion, adjacency matrix, and traversal methods.

--- graphs.py
+++ graphs.py
@@ -1,67 +1,109 @@
-from collections import deque
+# #adj_matrix ========================================
 
-def graph_insertion():
-    n = int(input())
-    m = int(input())
+# n = int(input())
+# matrix = []
 
-    #adj matrix------------------------------
-    
-    matrix = []
-    for i in range(n):
-        matrix.append([0]*n)
+# for i in range(n):
+#     row = list(map(int,input().split()))
+#     matrix.append(row)
 
-
-    for i in range(m):
-        u = int(input())
-        v = int(input())
-
-        matrix[u][v] = 1
-        matrix[v][u] = 1
-
-    #adj list------------------------------
-
-    arr = []
-    for i in range(n):
-        arr.append([])
+# for i in range(n):
+#     for j in range(n):
+#         print(matrix[i][j],end = " ")
+#     print()
 
 
-    for i in range(m):
-        u = int(input())
-        v = int(input())
-
-        arr[u].append(u)
-        arr[v].append(v)
-
-def dfs(node,adj,visit,ans):
-    visit[node] = 1
-    ans.append(node)
-
-    for x in adj[node]:
-        if not visit[x]:
-            dfs(x,adj,visit,ans)
-
-def bfs(v,adj):
-    n = len(adj)
-
-    visit = [0]*v
-    visit[0] = 1
-
-    ans = []
-
-    q = deque()
-    q.append(0)
-
-    while q:
-        node = q.popleft()
-        ans.append(node)
-
-        for x in adj[node]:
-            if not visit[x]:
-                visit[x] =1
-                q.append(x)
-
-    return ans
 
 
-        
 
+# adj_list ========================================
+
+# n, m = list(map(int,input().split()))
+
+# adj_list = []
+# for i in range(n):
+#     adj_list.append([])
+
+# for i in range(m):
+#     u, v = list(map(int,input().split()))
+#     adj_list[u].append(v)
+#     adj_list[v].append(u)
+
+# for i in range(n):
+#     print(i,"=",adj_list[i])
+
+
+
+
+
+# adj_list traversal ========================================
+
+# def bfs(adj_list):
+#     n = len(adj_list)
+
+#     visit =[0]*n
+#     q = deque()
+#     ans = []
+
+#     visit[0] = 1
+#     q.append(0)
+
+#     while q:
+
+#         node = q.popleft()
+#         ans.append(node)
+
+#         for x in adj_list[node]:
+#             if not visit[x]:
+#                 visit[x] = 1
+#                 q.append(x)
+
+#     return ans
+
+
+
+
+
+# def dfs(node,adj_list,visit,ans):
+#     visit[node] = 1
+#     ans.append(node)
+
+#     for x in adj_list[node]:
+#         if not visit[x]:
+#             dfs(x,adj_list,visit,ans)
+
+#     return ans
+
+
+
+
+
+# matrix to adj_list ========================================
+
+# adj_list =[]
+
+# for i in range(n):
+#     row = []
+
+#     for j in range(n):
+#         if matrix[i][j]==1:
+#             row.append(j)
+
+#     adj_list.append(row)
+
+
+
+
+
+# adj_list to matrix========================================
+
+# matrix = []
+# for i in range(n):
+#     rows = []
+#     for j in range(n):
+#         rows.append(0)
+#     matrix.append(rows)
+
+# for i in range(n):
+#     for j in adj_list[i]:
+#         matrix[i][j] =1
