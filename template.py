e16f7fb5a3cd71488631f468d6bbc3265e3819cf
Siddu Deshetti <143020292+siddudeshetti@users.noreply.github.com>
2026-07-30 00:47:33 +0530

    Create template.py for competitive programming
    Add a template for a competitive programming solution with input handling.

---
+++ template.py
@@ -0,0 +1,46 @@
+MOD = 1000000007
+INF = 10**18
+
+def solve():
+    
+    #single variables ================================
+
+    # n = int(input())
+    # s = input()      #s = input().split()
+
+    # array ==========================================
+
+    # arr = [0]*n
+    # s = input().split()
+
+    # for i in range(n):
+    #     arr[i] = int(s[i])
+
+    # for x in arr:
+    #         print(x,end = " ")
+
+    # map ============================================
+
+    # mp = {}
+
+    # for x in arr:
+    #     if x not in mp:
+    #         mp[x] = 0
+    #     mp[x] += 1
+
+    # for key, value in mp.items():
+    #     print(key, value)
+
+    # Algorithm ============================================
+    pass
+
+    
+
+
+
+# ==================== Driver ====================
+
+t = int(input())
+while t > 0:
+    solve()
+    t-=1
